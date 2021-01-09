5d2ec4f34ee4ea0ab8ac525d758a3178ecb41895
thub1706651@student.ctu.edu.vn <thub1706651@student.ctu.edu.vn>
2021-01-09 19:33:57 +0700

    vendor table(kh nv)

---
+++ database/migrations/2021_01_09_121424_create_khachhang_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKhachhangTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('khachhang', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->unsignedInteger('kh_ma')->comment('Mã khách hàng');
+            $table->string('kh_hoten', 100)->comment('Họ tên # Họ tên');
+            $table->string('kh_taikhoan', 50)->comment('Tài khoản # Tài khoản');
+            $table->string('kh_matkhau', 256)->comment('Mật khẩu # Mật khẩu');
+            $table->dateTime('kh_ngaySinh')->default(DB::raw('CURRENT_TIMESTAMP'))->comment('Ngày sinh # Ngày sinh');
+            $table->string('kh_diaChi', 250)->nullable()->default('NULL')->comment('Địa chỉ # Địa chỉ');
+            $table->unsignedTinyInteger('kh_gioiTinh')->default('1')->comment('Giới tính # Giới tính: 0-Nữ, 1-Nam, 2-Khác');
+            $table->string('kh_email', 100)->comment('Email # Email');
+            $table->string('kh_dienThoai', 11)->nullable()->default('NULL')->comment('Điện thoại # Điện thoại');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('khachhang');
+    }
+}
---
+++ database/migrations/2021_01_09_122713_create_nhanvien_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNhanvienTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nhanvien', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->smallIncrements('nv_ma')->comment('Mã nhân viên, 1-chưa phân công');
+            $table->string('nv_taiKhoan', 50)->comment('Tài khoản # Tài khoản');
+            $table->string('nv_matKhau', 256)->comment('Mật khẩu # Mật khẩu');
+            $table->string('nv_hoTen', 100)->comment('Họ tên # Họ tên');
+            $table->unsignedTinyInteger('nv_gioiTinh')->default('1')->comment('Giới tính # Giới tính: 0-Nữ, 1-Nam, 2-Khác');
+            $table->string('nv_email', 100)->comment('Email # Email');
+            $table->dateTime('nv_ngaySinh')->default(DB::raw('CURRENT_TIMESTAMP'))->comment('Ngày sinh # Ngày sinh');
+            $table->string('nv_diaChi', 250)->comment('Địa chỉ # Địa chỉ');
+            $table->string('nv_dienThoai', 11)->comment('Điện thoại # Điện thoại');
+            $table->timestamp('nv_taoMoi')->default(DB::raw('CURRENT_TIMESTAMP'))->comment('Thời điểm tạo # Thời điểm đầu tiên tạo nhân viên');
+            $table->timestamp('nv_capNhat')->default(DB::raw('CURRENT_TIMESTAMP'))->comment('Thời điểm cập nhật # Thời điểm cập nhật nhân viên gần nhất');
+            $table->tinyInteger('nv_trangThai')->default('2')->comment('Trạng thái # Trạng thái nhân viên: 1-khóa, 2-khả dụng');
+            $table->string('q_ten')->comment('Quyền # Ten quyền');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nhanvien');
+    }
+}
